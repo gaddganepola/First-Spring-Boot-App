026dec54185950f408d649b70a4bc8d89ac325db
gaddganepola <ganeypola@gmail.com>
2025-07-21 18:56:11 +0530

    dependency injection demo

--- src/main/java/com/springdemo/SpringBootFirstDemo/SpringBootFirstDemoApplication.java
+++ src/main/java/com/springdemo/SpringBootFirstDemo/SpringBootFirstDemoApplication.java
@@ -2,13 +2,26 @@ package com.springdemo.SpringBootFirstDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootFirstDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootFirstDemoApplication.class, args);
+		Alien obj1;
 
-		System.out.println("Hello World!");
+//		Manually create an object
+//		obj1 = new Alien();
+
+//		Use Application Context to create an object
+		ApplicationContext context = SpringApplication.run(SpringBootFirstDemoApplication.class, args);
+
+		obj1 = context.getBean(Alien.class);
+
+		obj1.code();
+
+		Alien obj2 = context.getBean(Alien.class);
+		obj2.code();
+
 	}
 }
